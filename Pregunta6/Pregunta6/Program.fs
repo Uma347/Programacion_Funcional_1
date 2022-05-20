971e6dacd5af15a8a7e71abb86dbf05509fecece
Uma347 <75706316+Uma347@users.noreply.github.com>
2022-05-20 03:20:23 -0400

    Add files via upload

---
+++ Pregunta6/Pregunta6/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+
+let suma a b = a + b
+let resta a b = a - b
+let multiplicacion a b = a * b
+let division a b = System.Math.Round(float(a/b),5);
+
+[<EntryPoint>]
+let main argumentos = 
+    Console.Write("Introduzca el 1er numero: ")
+    let n1 = Convert.ToDouble(Console.ReadLine())
+    Console.Write("Introduzca el 2do numero: ")
+    let n2 = Convert.ToDouble(Console.ReadLine())
+    Console.WriteLine()
+    Console.WriteLine("Resultado de las operaciones:")
+    Console.WriteLine(n1.ToString()+" + "+n2.ToString()+" = "+(suma n1 n2).ToString())
+    Console.WriteLine(n1.ToString()+" - "+n2.ToString()+" = "+ (resta n1 n2).ToString())
+    Console.WriteLine(n1.ToString()+" * "+n2.ToString()+" = "+(multiplicacion n1 n2).ToString())
+    if (n2 = 0.0) then 
+            Console.WriteLine("No existe division entre cero")
+        else 
+            Console.WriteLine(n1.ToString()+" / "+n2.ToString()+" = "+(division n1 n2).ToString())
+    let f = Console.ReadKey()
+    0 
